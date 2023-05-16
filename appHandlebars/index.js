d0c1e88dd54c0731faac74d2495f430064b275f7
victoricoma <victorsena3010@gmail.com>
2023-05-16 20:31:43 -0300

    Aula1605 Template Engine if e else

--- appHandlebars/index.js
+++ appHandlebars/index.js
@@ -6,14 +6,29 @@ const app = express() //construtor de de pack
 
 app.engine('handlebars', exphbs())
 app.set('view engine','handlebars')
+//Encode de json para express
+app.use(express.urlencoded({extended: true,}),)
+app.use(express.json())
 
-app.get('/', function (req, res){
-    const user ={
-        name:"Victor Hugo",
-        surname:"Icoma",
-        age:"41"
+app.post('/verifyUser', (req, res) =>{
+    const form={
+        name:req.body.name,
+        surname:req.body.surname,
+        age:req.body.age,
+        approved:false
     }
-    res.render('home',{user:user})
+    if(form.age>=32){
+        form.approved=true
+    }else{
+        form.approved=false
+    }
+    res.status(201).render('home',{user:form})
+})
+app.get('/', function (req, res){
+    res.status(200).render('home')
+})
+app.get('/dashboards', function(req, res){
+    res.render('dashboards')
 })
 
 app.listen(3000)
